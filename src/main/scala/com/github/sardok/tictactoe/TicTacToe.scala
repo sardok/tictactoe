8c2ae341aca8f4d82a6c12e9398d1fac1dfb8b23
Sinan Nalkaya <sardok@gmail.com>
2014-12-12 22:11:35 +0200

    Removed looping logic from the application.

--- src/main/scala/com/github/sardok/tictactoe/TicTacToe.scala
+++ src/main/scala/com/github/sardok/tictactoe/TicTacToe.scala
@@ -1,29 +1,14 @@
 package com.github.sardok.tictactoe
+import akka.actor.{Props, ActorSystem}
+
 
 object TicTacToe extends App {
 
-  val board = new Board(3, 3)
-  val printer = new ConsolePrinter
-  val game = new Game(Human, Machine, board, printer)
+  val system = ActorSystem("tictactoe")
+  val coordinator = system.actorOf(Props[Coordinator])
 
-  getChar("Welcome to TicTacToe, press any key to start . . .")
+  coordinator ! StartGame
 
-  while (!game.isEnd) {
-    printer.print(board)
-    try {
-      game.step
-    } catch {
-      case StopGameException(msg) =>
-        printer.print(msg)
-        printer.print(board)
-        game.end
-    }
-  }
+  system.awaitTermination()
 
-  def getChar(msg: String = "") = {
-    if (!msg.isEmpty) {
-      printer.print(msg)
-    }
-    Console.in.read.toChar
-  }
 }
